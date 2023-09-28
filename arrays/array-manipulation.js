32dad0c0ccabbaa03fe306ccb14c3c1c93b14580
Miguel Atencio Vargas <atencio947@gmail.com>
2023-09-28 16:23:13 -0400

    Add optimized solution for array manipulation

--- arrays/array-manipulation.js
+++ arrays/array-manipulation.js
@@ -15,33 +15,27 @@ function arrayManipulation(n, queries) {
   // 3. Read the queries and add the range of a  to b with K
   queries.forEach((row) => {
     const [i, j, k] = row;
-    for (let index = i - 1; index < j; index++) {
+    for (let index = i; index < j; index++) {
       const value = arr[index] + k;
       arr[index] = value;
       if (value > max) max = value;
     }
-    console.log({
-      arr,
-      max
-    });
   });
-
-
-  return max;
+    
 }
-
 
 // arrayManipulation(10, [
 //   [1, 5, 3],
 //   [4, 8, 7],
 //   [6, 9, 1]
 // ])
-arrayManipulation(10, [
-  [2, 6, 8],
-  [3, 5, 7],
-  [1, 8, 1],
-  [5, 9, 15]
-])
+// arrayManipulation(10, [
+//   [2, 6, 8],
+//   [3, 5, 7],
+//   [1, 8, 1],
+//   [5, 9, 15]
+// ])
+
 // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -49,3 +43,44 @@ arrayManipulation(10, [
 // [0, 8, 15, 15, 15, 8, 0, 0, 0, 0] // 3, 5, 7
 // [1, 9, 16, 16, 16, 9, 1, 1, 0, 0] // 1, 8, 1
 // [1, 9, 16, 16, 31, 24, 16, 16, 15, 0] // 5, 9, 15
+
+// In order to optimize the algorithm that is running in O(n^2)
+// analize the problem of prefix sum; this is to sum of: 
+const input = [1, 2, 3, 4, 5]
+const output = []; // [1, 3, 6, 10, 15]
+
+for (let i = 0; i < input.length; i++) {
+  let sum = 0;
+  for (let j = 0; j <= i; j++) {
+    sum = sum + input[j]
+  }
+  output[i] = sum;
+}
+// console.log('⛳️ | output:', output);
+
+// Again this is O(n^2)
+// we can optimize to O(n) like this:
+
+const prefixSum = [input[0]];
+
+for (let i = 1; i < input.length; i++) {
+  prefixSum[i] = prefixSum[i - 1] + input[i];
+}
+// console.log('⛳️ | prefixSum:', prefixSum);
+
+function arrayManipulationV2(n, queries) {
+  const arr = Array(n + 1).fill(0);
+  queries.forEach((row) => {
+    const [a, b, k] = row;
+    arr[a - 1] += k;
+    arr[b] -= k;
+  });
+
+  let max = 0;
+  let curr = 0
+  arr.forEach((num) => {
+    curr += num;
+    max = Math.max(curr, max);
+  });
+  return max;
+}
