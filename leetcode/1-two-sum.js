ed4928f71490e63f9847272c618d2436a4544f4b
Miguel Atencio Vargas <atencio947@gmail.com>
2023-10-02 19:53:42 -0400

    Add O(2n) solution for two sum

--- leetcode/1-two-sum.js
+++ leetcode/1-two-sum.js
@@ -3,7 +3,7 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+var twoSumOld = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     const a = nums[i];
     for (let j = i + 1; j < nums.length; j++) {
@@ -15,3 +15,27 @@ var twoSum = function (nums, target) {
   }
 };
 
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map = nums.reduce((acc, value, i) => {
+    acc.set(value, i);
+    return acc;
+  }, new Map());
+
+  for (let j = 0; j < nums.length; j++) {
+    const complement = target - nums[j];
+    const index = map.get(complement);
+    if (index && index != j) {
+      return [j, index];
+    }
+  }
+}
+
+
+console.log(twoSum([2, 11, 15, 7], 9))
+// console.log(twoSum([3, 3], 6))
