d3feef66497836006bd1d785fd37e78b137cf9f6
Miguel Atencio Vargas <atencio947@gmail.com>
2023-10-05 23:08:20 -0400

    Improve solution longest common prefix problem

--- leetcode/14-longest-common-prefix.js
+++ leetcode/14-longest-common-prefix.js
@@ -2,10 +2,10 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function (strs) {
+var longestCommonPrefixOld = function (strs) {
   const len = strs.length;
   const map = new Map();
-  
+
   for (let word of strs) {
     for (let i = 0; i < word.length; i++) {
       const subStr = word.substring(0, word.length - i);
@@ -14,13 +14,34 @@ var longestCommonPrefix = function (strs) {
       if (count === len) return subStr;
     }
   }
-
-
 };
 
+var longestCommonPrefix = function (strs) {
+  const len = strs.length;
+  const sortedStrings = strs.sort();
+  console.log('⛳️ | longestCommonPrefix | strs:', strs);
+  console.log('⛳️ | longestCommonPrefix | sortedStrings:', sortedStrings);
 
-const strings = ["cir","car"];
-const result = longestCommonPrefix(strings);
+  const firstString = sortedStrings[0];
+  console.log('⛳️ | longestCommonPrefix | firstString:', firstString);
+  const lastString = sortedStrings[len - 1];
+  console.log('⛳️ | longestCommonPrefix | lastString:', lastString);
 
-console.log('⛳️ | result:', result);
-console.log('⛳️ | pass: ', result === "c" ? "✅" : "🚨");
+
+
+  for (let i = 0; i < firstString.length; i++) {
+    const subStr = firstString.substring(0, firstString.length - i);
+
+    if (lastString.substring(0, subStr.length) == subStr) {
+      console.log('⛳️ | longestCommonPrefix | subStr:', subStr);
+      return subStr;
+    }
+  }
+  return "";
+}
+
+
+console.log("CASE 1: ", longestCommonPrefix(["flower", "flow", "flight"]) === "fl" ? "✅" : "🚨");
+console.log("CASE 2: ", longestCommonPrefix(["reflower", "flow", "flight"]) === "" ? "✅" : "🚨");
+console.log("CASE 3: ", longestCommonPrefix(["carman", "ca", "car"]) === "ca" ? "✅" : "🚨");
+console.log("CASE 4: ", longestCommonPrefix(["abab", "aba", "abc"]) === "ab" ? "✅" : "🚨");
