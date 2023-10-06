82132783fad7938b8ac0fbf15a123fbd0d925884
Miguel Atencio Vargas <atencio947@gmail.com>
2023-10-06 14:34:25 -0400

    Add solution for valid parentheses problem

---
+++ leetcode/20-valid-parentheses.js
@@ -0,0 +1,44 @@
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  const stack = [];
+
+  for (let char of s) {
+    if (char == "(" || char == "{" || char == "[") {
+      stack.push(char);
+    } else {
+      const last = stack[Math.max(stack.length - 1, 0)];
+      let pop = false;
+      if (last == "(" && char == ")") pop = true;
+      if (last == "{" && char == "}") pop = true;
+      if (last == "[" && char == "]") pop = true;
+
+      if (pop) stack.pop();
+      else return false;
+    }
+  };
+  return s.length > 1 && stack.length == 0;
+};
+
+let c = 1;
+
+let result = isValid("()");
+console.log(`${c++}:`, result, result === true ? "✅" : "🚨",);
+
+result = isValid("()[]{}");
+console.log(`${c++}:`, result, result === true ? "✅" : "🚨");
+
+result = isValid("(]");
+console.log(`${c++}:`, result, result === false ? "✅" : "🚨");
+
+result = isValid("")
+console.log(`${c++}:`, result, result === false ? "✅" : "🚨");
+
+result = isValid("(({}))")
+console.log(`${c++}:`, result, result === true ? "✅" : "🚨");
+
+result = isValid("((")
+console.log(`${c++}:`, result, result === false ? "✅" : "🚨");
